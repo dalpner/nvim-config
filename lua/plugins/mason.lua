eea2aae6dded94e440d10ca0ca37d43c8fd7e38b
Dominic Leipner Alvarez <dominicleipner@gmx.de>
2023-09-06 12:10:09 +0200

    refactoring plugins

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "lua-language-server",
+        "gopls",
+        "golangci-lint",
+      },
+    },
+  },
+}
